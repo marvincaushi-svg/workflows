1d9520efcdacafbaf519ef7779d3e6e15e1bce8e
marvincaushi-svg <marvin.caushi@gmail.com>
2026-08-03 13:59:29 +0200

    Create technical evidence request

--- workflowos/__init__.py
+++ workflowos/__init__.py
@@ -2,12 +2,13 @@
 
 from .audit import build_audit_log, read_audit_log, verify_audit_log, write_audit_log
 from .core import WorkflowError, build_case_from_email, evaluate_case, load_document
-from .technical_review import evaluate_technical_review
+from .technical_review import create_remediation_work, evaluate_technical_review
 
 __all__ = [
     "WorkflowError",
     "build_audit_log",
     "build_case_from_email",
+    "create_remediation_work",
     "evaluate_case",
     "evaluate_technical_review",
     "load_document",
