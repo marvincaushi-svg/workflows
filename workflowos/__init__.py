67e21479329486249d4906e9dc96bc6f809900b6
marvincaushi-svg <marvin.caushi@gmail.com>
2026-08-03 21:19:36 +0200

    Split A&F work and gate SB document delivery

--- workflowos/__init__.py
+++ workflowos/__init__.py
@@ -2,15 +2,22 @@
 
 from .audit import build_audit_log, read_audit_log, verify_audit_log, write_audit_log
 from .core import WorkflowError, build_case_from_email, evaluate_case, load_document
-from .technical_review import create_remediation_work, evaluate_technical_review
+from .technical_review import (
+    create_remediation_plan,
+    create_remediation_work,
+    evaluate_technical_review,
+    record_monday_document_notification,
+)
 
 __all__ = [
     "WorkflowError",
     "build_audit_log",
     "build_case_from_email",
     "create_remediation_work",
+    "create_remediation_plan",
     "evaluate_case",
     "evaluate_technical_review",
+    "record_monday_document_notification",
     "load_document",
     "read_audit_log",
     "verify_audit_log",
